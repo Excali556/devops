00b96e72302af32590109d71c5a2e6e6d616852e
Aditya Mallikarjun <excali556@gmail.com>
2026-04-01 10:40:25 +0530

    Update javaa.java

--- javaa.java
+++ javaa.java
@@ -1,4 +1,5 @@
-class gfg{
-  public static void main(String[]args){
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
